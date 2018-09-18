fce499c78674d80450b107fa4ae1001bd3d63ed0
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2018-09-18 12:19:56 -0600

    fixing area

--- src/kyleeprescottch3examples/ComputeArea.java
+++ src/kyleeprescottch3examples/ComputeArea.java
@@ -10,49 +10,26 @@ public class ComputeArea {
 
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
-		String adjective;
-		 String name;
-		 String number;
-		 String relative;
-		 String place; 
-		 String place2;
-		 String verbing;
-		 String pluralNoun;
-		 String adjective2;
-		 String adjective3;
-		 String person; 
-		 String adjectively;
-		 System.out.println("enter adjective");
-		 adjective = input.next();
-		 System.out.println("enter name");
-		 name = input.next();
-		 System.out.println("eneter number");
-		 number = input.next();
-		 System.out.println("enter relative");
-		 relative=input.next();
-		 System.out.println("eneter place");
-		 place=input.next();
-		 System.out.println("enter place2");
-		 place2 =input.next();
-		 System.out.println("eneter verbing");
-		 verbing=input.next();
-		 System.out.println("pluarNoun");
-		 pluralNoun= input.next();
-		 System.out.println("eneter adjective2 ");
-		 adjective2 = input.next();
-		 System.out.println("enter adjective3");
-		 adjective3 = input.next();
-		 System.out.println("eneter person");
-		 person = input.next();
-		 System.out.println("enter adjectively");
-		 adjectively = input.next();
+		
+		//step one:enter the radius of a circle
+		//constant declinations 
+		  double radius;
+		  double PI= 3.1415926535979323846264338327950288419716939;
+		  double area; 
+		  
+		  //assign a value to the radius 
+		  System.out.println("Eneter the radius of a cirlce and press enter");
+		  
+		  radius = input.nextDouble();
 		 
-	System.out.println("A new "+adjective+ " fairy princess is coming out soon! It will be about Snow "+name+" and the"+number+" "
-			+ " dwarfs. Snow "+name+" is a princess whose beauty threatens her"+relative+" the queen.Snow "+name+" is forced to flee from"+place+""
-					+ "and hides in nearby "+place2+"There she discovers the dwarfs"+verbing+"in their "+pluralNoun+"But the queen finds her and casts a"
-							+ adjective2+"spell on her The dwarfs take care of her until the"+adjective3+""+person+" comes to rescue her "
-									+ "and they all live "+adjectively+"ever after ");
-		 
-		 
-	}
+		  
+		//step two:compute the area using the following formula:
+		//area=radius*radius* pi 
+		area=radius*radius*PI;
+		
+ 		//step three:display result
+		
+		System.out.println("The area of your circle is:"+ area +"\"");
+ 	}
+
 }
--- src/kyleeprescottch3examples/Madlib.java
+++ src/kyleeprescottch3examples/Madlib.java
@@ -1,7 +1,56 @@
 package kyleeprescottch3examples;
+
+import java.util.Scanner;
+
 /*
  * 
  */
 public class Madlib {
-
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		String adjective;
+		 String name;
+		 String number;
+		 String relative;
+		 String place; 
+		 String place2;
+		 String verbing;
+		 String pluralNoun;
+		 String adjective2;
+		 String adjective3;
+		 String person; 
+		 String adjectively;
+		 System.out.println("enter adjective");
+		 adjective = input.next();
+		 System.out.println("enter name");
+		 name = input.next();
+		 System.out.println("eneter number");
+		 number = input.next();
+		 System.out.println("enter relative");
+		 relative=input.next();
+		 System.out.println("eneter place");
+		 place=input.next();
+		 System.out.println("enter place2");
+		 place2 =input.next();
+		 System.out.println("eneter verbing");
+		 verbing=input.next();
+		 System.out.println("pluarNoun");
+		 pluralNoun= input.next();
+		 System.out.println("eneter adjective2 ");
+		 adjective2 = input.next();
+		 System.out.println("enter adjective3");
+		 adjective3 = input.next();
+		 System.out.println("eneter person");
+		 person = input.next();
+		 System.out.println("enter adjectively");
+		 adjectively = input.next();
+		 
+	System.out.println("A new "+adjective+ " fairy princess is coming out soon! It will be about Snow "+name+" and the"+number+" "
+			+ " dwarfs. Snow "+name+" is a princess whose beauty threatens her"+relative+" the queen.Snow "+name+" is forced to flee from"+place+""
+					+ "and hides in nearby "+place2+"There she discovers the dwarfs"+verbing+"in their "+pluralNoun+"But the queen finds her and casts a"
+							+ adjective2+"spell on her The dwarfs take care of her until the"+adjective3+""+person+" comes to rescue her "
+									+ "and they all live "+adjectively+"ever after ");
+		 
+		 
+	}
 }
